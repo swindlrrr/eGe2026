5db2954a32a3fc20e9d4c2cc1405c52d29f50d88
swindlrrr <guzagame2569@gmail.com>
2026-02-07 19:35:17 +0200

    task 5 theory

--- Task-5/theory.py
+++ Task-5/theory.py
@@ -11,6 +11,10 @@ print(hex(num)[2:])
 print(f'{num:x}')
 
 
+# Десятеричная система
+# int(num,sys) sys - система из которой переводим
+
+
 # Перевод в любую систему счисления (2<= sys <=9)
 def convert(num, sys):  # num = 20, sys = 2
     res = ''
@@ -46,3 +50,34 @@ print(sum(map(int, num_bin)))
 
 # Сумма цифр любой системы (2<= sys <= 36)
 print(sum(map(lambda x: int(x, 36), num_bin)))
+
+# Замена символов в строке
+# Заменить N символов слева R[N:]
+# Заменить N символов справа R[:-N]
+
+# Заменить все символы в строке (все 0 на 1)
+R = '1010'
+R = R.replace('0', '1')
+
+# Заменить все символы в строке (все 1 на 3, а 3 на 1)
+R = R.replace('1', '*')
+R = R.replace('3', '1')
+R = R.replace('*', '1')
+
+# Усложненные варианты вопросов
+# Максимальное N при максимальном R
+ans.append([R, N])
+print(max(ans))
+# Минимальное N при минимальном R
+ans.append([R, N])
+print(min(ans))
+
+# Минимальное N при максимальном R
+ans.append([R, N])
+ans = sorted(ans, key=lambda x:(-x[0], x[1]))
+print(ans[0])
+
+# Максимальное N при минимальном R
+ans.append([R, N])
+ans = sorted(ans, key=lambda x:(x[0], -x[1]))
+print(ans[0])
