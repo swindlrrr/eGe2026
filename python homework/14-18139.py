6f3d18e8e34167b195b61fe07019bb34f30bcc13
swindlrrr <guzagame2569@gmail.com>
2026-02-18 19:46:04 +0200

    homework

--- python homework/14-18139.py
+++ python homework/14-18139.py
@@ -1,9 +1,14 @@
-ans = []
 from string import printable
-for x in printable[:25]:
-    num1 = int(f'8af7{x}11', 25)
-    num2 = int(f'{x}da87', 25)
-    num = num1 + num2
-    for Y in range(1,100_000):
-        if Y <= 100:
-            s = num%Y == 0
+
+cnt = 0
+for Y in range(1,101):
+    for x in printable[1:25]:
+        num1 = int(f'8af7{x}11', 25)
+        num2 = int(f'{x}da87', 25)
+        num = num1 + num2
+        if num % Y ==0:
+            cnt += 1
+            break
+print(cnt)
+
+
