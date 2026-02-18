16086cac7a6c85a5385151cf23bf50c3afdd1070
swindlrrr <guzagame2569@gmail.com>
2026-02-18 19:46:18 +0200

    task 5

--- Task-5/task-21404.py
+++ Task-5/task-21404.py
@@ -1,7 +1,7 @@
 ans = []
 for N in range(1, 100_000):
     R = bin(N)[2:]
-    if N % 2 == 0:
+    if R.count('1') % 2 == 0:
         R = '10' + R[2:] + '0'
     else:
         R = '11' + R[2:] + '1'
