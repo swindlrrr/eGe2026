67612fb8ddc2d8e65e6be662287fcc9904b7fba9
swindlrrr <guzagame2569@gmail.com>
2026-02-19 23:05:44 +0200

    school hw

--- school hw/20-02-26.py
+++ school hw/20-02-26.py
@@ -11,44 +11,41 @@ N = convert(num, 5)
 N = N.count('2')
 print(N)
 
-
 from string import printable
 
 cnt = 0
-for Y in range(1,101):
+for Y in range(1, 101):
     for x in printable[1:25]:
         num1 = int(f'8af7{x}11', 25)
         num2 = int(f'{x}da87', 25)
         num = num1 + num2
-        if num % Y ==0:
+        if num % Y == 0:
             cnt += 1
             break
 print(cnt)
 
-
-
 from string import printable
+
 for x in printable[:19]:
     num1 = int(f'98897{x}21', 19)
     num2 = int(f'2{x}923', 19)
     num = num1 + num2
-    if num % 18 ==0:
-        print(num//18)
+    if num % 18 == 0:
+        print(num // 18)
 
 
-
-def convert (num,sys):
+def convert(num, sys):
     res = ''
     while num:
-        res +=str(num%sys)
+        res += str(num % sys)
         num //= sys
     return res[::-1]
-num = 3*3125**8 + 2*625**7 - 4*625**6 + 3*125**5 - 2*25**4 - 2024
-N = convert(num,25)
+
+
+num = 3 * 3125 ** 8 + 2 * 625 ** 7 - 4 * 625 ** 6 + 3 * 125 ** 5 - 2 * 25 ** 4 - 2024
+N = convert(num, 25)
 z = N.count('0')
 print(z)
-
-
 
 from string import printable
 
@@ -64,7 +61,6 @@ def convert(num, sys):
 for N in range(2, 37):
     if convert(41, N)[-1] == '2' and convert(131, N)[-1] == '1':
         print(N)
-
 
 
 def convert(num, sys):
